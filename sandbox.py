37ffef0227468af7dbd2f6ba9fe6ab40906910b0
gbrown <georg.braunbeck@googlemail.com>
2018-02-21 16:18:13 +0100

    formed loading data into functions

--- sandbox.py
+++ sandbox.py
@@ -1,6 +1,7 @@
 from constants import *
 
-b = 1.5  # Gauss
+b = 600  # Gauss
 f = 2 * MU_B * b*1e-4 / H * 1e-9
 print('P1:', f)
-print('NV:', 2.87 - f)
+print('NV low:', 2.87 - f)
+print('NV high: ', 2.87 + f)
